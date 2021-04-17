d0b13b1c15da0f96f60721a0b805857d87456df6
Anna Freimuth <a.freimuth90@gmail.com>
2021-04-17 09:09:53 +0200

    modified js

--- 2021_04_15_js_intro/js/main.js
+++ 2021_04_15_js_intro/js/main.js
@@ -3,3 +3,17 @@ console.log("Hello world")
 let a;  //undefined
 let b = a;  //undefined
 let c = null;
+
+function f1(){
+    console.log("inside f1");
+}
+
+const d = 10;
+let f2 = function f2(number){
+    return number * 2;
+}
+let e = f2(d)
+
+console.log(`e equals to ${e}`)
+e = f2;
+console.log(e(d))
