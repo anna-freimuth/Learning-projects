9e9cac514af973643dd9a8a0379ce48e000e4c91
Anna Freimuth <a.freimuth90@gmail.com>
2021-04-21 20:31:06 +0200

    dom elements

---
+++ 2021_04_21_dom_element/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    let marsText = document.querySelector(".mars")
+    marsText.id = "first-text"
+    console.log(marsText.id)
+}
