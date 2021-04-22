44c082197c50934eeb72a27c1e137b1165d2ab79
Anna Freimuth <a.freimuth90@gmail.com>
2021-04-22 11:09:44 +0200

    event listener

--- 2021_04_21_dom_element/main.js
+++ 2021_04_21_dom_element/main.js
@@ -1,7 +1,12 @@
-document.addEventListener("DOMContentLoaded", main);
-
-function main() {
+document.addEventListener("DOMContentLoaded",function(){
     let marsText = document.querySelector(".mars")
     marsText.id = "first-text"
     console.log(marsText.id)
-}
+
+    //make text red/non red while clicking on it
+    marsText.addEventListener("click",function (event){
+        marsText.classList.toggle("red");
+       // event.target.classList.toggle("red");         //if there is no link
+    });
+});
+
